6ce6efbad18c8606ec4a883d6f387375142dc921
Rodrigo Herrera <ehn.rodrigo@gmail.com>
2026-06-10 19:52:57 -0600

    Migration completed

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { loginUser, registerAdmin } = require("../controllers/authController");
-
-router.post("/register", registerAdmin);
-router.post("/login", loginUser);
-
-module.exports = router;
